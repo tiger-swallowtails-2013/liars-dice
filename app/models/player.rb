42590536570b57dbee6ddef57949b7d6dcd1abab
nathanallen <thenathanator@gmail.com>
2013-10-01 10:14:00 -0700

    fix player custom validate bug and roll method bug

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,20 +4,25 @@ class Player < ActiveRecord::Base
   after_save :check_lie
 
   def roll
-    self.current_roll = ""
-    6.times {self.current_roll << rand(6) + 1}
+    rolls = []
+    6.times {rolls << rand(6) + 1}
+    self.current_roll = rolls.sort.join
   end
 
   private
 
   def claim_format
-    if !(current_claim =~ /[1-5]{1}x[1-6]{1}/)
-      errors.add(:current_claim, "can't be in that format")
+    unless current_claim == nil
+      if !( current_claim =~ /[1-5]{1}x[1-6]{1}/)
+        errors.add(:current_claim, "can't be in that format")
+      end
     end
   end
 
   def check_lie
-    match = /[#{current_claim[2]}]{#{current_claim[0]}}/
-    (current_roll.to_s =~ match).nil? ? self.bullshit = true : self.bullshit = false
+    unless current_claim == nil
+      match = /[#{current_claim[2]}]{#{current_claim[0]}}/
+      (current_roll.to_s =~ match).nil? ? self.bullshit = true : self.bullshit = false
+    end
   end
 end
