f27b2f3df951313717f474d4c9260cbd85a81da5
nathanallen <thenathanator@gmail.com>
2013-09-30 17:27:01 -0700

    validates format of user input

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,11 @@
 class Player < ActiveRecord::Base
   belongs_to :game
+  validate :claim_format
+
+  def claim_format
+    if !(current_claim =~ /[1-5]{1}x[1-6]{1}/)
+      errors.add(:current_claim, "can't be in that format")
+    end
+  end
 
 end
