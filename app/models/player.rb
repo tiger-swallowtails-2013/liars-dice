9092d0d7639cdcdc5c899bd94b7230e6f054d692
nathanallen <thenathanator@gmail.com>
2013-09-30 18:21:09 -0700

    WIP: before_update check for bullshit

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,19 @@
 class Player < ActiveRecord::Base
   belongs_to :game
   validate :claim_format
+  before_update :check_lie
+
+
+  private
 
   def claim_format
     if !(current_claim =~ /[1-5]{1}x[1-6]{1}/)
       errors.add(:current_claim, "can't be in that format")
+    end
+  end
+
+  def check_lie
+    if current_claim
     end
   end
 
